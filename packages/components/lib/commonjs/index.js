7016c232a4ab2bee7af8f7a91158062c24add1d9
Olga Mozejko <o.mozejko@gmail.com>
2025-10-30 07:50:20 +0100

    setup of workspace + turborepo

---
+++ packages/components/lib/commonjs/index.js
@@ -0,0 +1,8 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.version = void 0;
+const version = exports.version = '0.1.0';
+//# sourceMappingURL=index.js.map
